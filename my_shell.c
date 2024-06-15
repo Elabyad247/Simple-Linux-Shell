4e814a6350432b5b4813d17753f6ed898c0deee3
Elabyad247 <elabyad247@gmail.com>
2024-06-15 13:07:07 +0300

    finished part 1

--- my_shell.c
+++ my_shell.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 #define MAX_INPUT_SIZE 1024
 #define MAX_TOKEN_SIZE 64
@@ -47,25 +48,48 @@ int main(int argc, char *argv[]) {
         printf("$ ");
         scanf("%[^\n]", line);
         getchar();
-
-        printf("Command entered: %s (remove this debug output later)\n", line);
         /* END: TAKING INPUT */
 
+        // Handle empty input
+        if (strcmp(line, "") == 0) {
+            continue;
+        }
         line[strlen(line)] = '\n'; //terminate with new line
         tokens = tokenize(line);
-
-        //do whatever you want with the commands, here we just print them
-
-        for (i = 0; tokens[i] != NULL; i++) {
-            printf("found token %s (remove this debug output later)\n", tokens[i]);
+        // Handle empty input after tokenization
+        if (tokens[0] == NULL) {
+            continue;
         }
 
+        // Handle  commands
+        if (strcmp(tokens[0], "cd") == 0) {
+            if (tokens[1] != NULL) { // Check if the directory is provided
+                if (chdir(tokens[1]) != 0) {
+                    printf("Error: Directory not found\n");
+                }
+            } else {
+                printf("Shell: Incorrect command\n");
+            }
+        } else if (strcmp(tokens[0], "exit") == 0) {
+            exit(0);
+        } else {
+            pid_t pid = fork();
+            if (pid < 0) {
+                printf("Shell: Fork failed");
+            } else if (pid == 0) {
+                if (execvp(tokens[0], tokens) == -1) { // Execute the command
+                    printf("Error: Command not found\n");
+                    exit(EXIT_FAILURE);
+                }
+            } else {
+                wait(NULL);
+            }
+        }
         // Freeing the allocated memory
         for (i = 0; tokens[i] != NULL; i++) {
             free(tokens[i]);
         }
         free(tokens);
-
     }
     return 0;
 }
